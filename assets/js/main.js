355371811d85f8652ba0758e19645355e719ffac
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-03-23 15:00:04 +0100

    complete with variable let

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+//Chiedi all’utente il suo nome,
+//poi chiedi il suo cognome,
+//poi chiedi il suo colore preferito
+//Infine scrivi sulla pagina il risultato in questo formato : nomecognomecolorepreferito22
+
+let nome;
+let cognome;
+let colore;
+let password;
+
+nome = prompt('Inserisci il tuo nome');
+cognome = prompt('Inserisci il tuo cognome');
+colore = prompt('Inserisci il tuo colore preferito');
+password = nome + cognome + colore + 22;
+document.getElementById('password').innerHTML = `La tua password è ${password}`;
+
+
+console.log(`La tua password è ${password}`);
+
